2e722edcc054c7c406a8234c9f8a410d8692ebff
Shrijita <162959749+shri202315@users.noreply.github.com>
2024-03-18 22:15:52 +0530

    20th code

---
+++ CodeChef/Course/LBCL02_problems_CLB011.c
@@ -0,0 +1,16 @@
+/*Rearrange the code to produce this output:
+#include <stdio.h>
+  printf("World");
+  printf("Hello\n");
+int main() {
+}
+  
+Hello
+World
+*/
+
+#include <stdio.h>
+int main() {
+  printf("Hello\n");
+  printf("World");
+}
