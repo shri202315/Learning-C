f5911bd0b693e9371efba42b25d977e024324399
Shrijita <162959749+shri202315@users.noreply.github.com>
2024-03-18 21:10:43 +0530

    5th code

---
+++ CodeChef/Course/LBCL02_problems_CLB009.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Add the formula for area of 11 and 13
+	printf("%d\n", 11*13);
+	
+	// Add the formula for perimeter of 11 and 13
+	printf("%d\n", 2* (11+13));
+}
+
