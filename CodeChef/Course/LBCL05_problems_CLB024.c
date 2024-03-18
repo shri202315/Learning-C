ee3f873aa4c5bb917cf30e2bfc9e9fca8e45ca3e
Shrijita <162959749+shri202315@users.noreply.github.com>
2024-03-18 21:43:58 +0530

    13th code

---
+++ CodeChef/Course/LBCL05_problems_CLB024.c
@@ -0,0 +1,14 @@
+//To sum a and b, you can print a + b, like we did with numbers previously.
+
+#include <stdio.h>
+
+int main() {
+	// Assign the value 23 to a
+    int a = 23;
+    
+    // Assign the value 20 to b
+    int b = 20;
+    
+    // Output the sum of a and b
+    printf("%d", a + b);
+}
